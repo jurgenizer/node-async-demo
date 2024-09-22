25946b3427e503a0746305e14ecec0abdbe12913
Jurgen <jurgen.geitner@gmail.com>
2024-09-22 18:35:22 +0200

    Callback example

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+//Patterns to  deal with asynchronous code
+// Callbacks
+// Promises
+// Async/Await
+
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from a database...');
-}, 2000)
+getUser(1, (user) => {
+    //console.log('The user: ', user);
+    getRepositories(user.gitHubUsername, (repos) => {
+        console.log('The repos: ', repos);
+    })
+});
 console.log('After');
+
+function getUser(id, callback) {
+    //simulate a long-running operation
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        callback({ id: id, gitHubUsername: 'jurgen' });
+    }, 2000)
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        callback({ username, repositories: ['repo1', 'repo2', 'repo3'] });
+    }, 2000)
+}
