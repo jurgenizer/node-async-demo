17b6adec2af30f65078e9f201e8af90acd096229
Jurgen <jurgen.geitner@gmail.com>
2024-09-25 16:40:38 +0200

    Async and Await syntactical sugar

--- index.js
+++ index.js
@@ -5,12 +5,32 @@
 
 console.log('Before');
 
-
+// Original Promised-based approach
 getUser(1)
 .then(user => getRepositories(user.gitHubUsername))
 .then(repos => getCommits(repos[0]))
 .then(commits => console.log('Commits', commits))
 .catch(err => console.log('Error:', err.message));
+
+// Modified Async and Await approach
+// Whenever you use the Await operator in a function, you have to decorate that function with the Async modifier
+// And the way we get the errors is using the Try..Catch block
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos  = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log('Commits from the syntactical suger async/await function:', commits);
+    } 
+    catch (err) {
+         console.log('Error:', err.message)
+    }
+   
+}
+
+// Look at the return type of the functiion (hint: Promise<void>)
+displayCommits();
+
 
 
 console.log('After');
@@ -39,7 +59,8 @@ function getRepositories(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('Calling GitHub API...');
-            resolve({ username, repositories: ['repo1', 'repo2', 'repo3'] });
+            //resolve({ username, repositories: ['repo1', 'repo2', 'repo3'] });
+            reject(new Error('Could not get the repos'));
         }, 2000)
     })
 }
--- promise-api.js
+++ promise-api.js
@@ -1,9 +1,41 @@
 // Create a Promise that is already resolved
-
-const promiseResolved = Promise.resolve({id:1});
-promiseResolved.then(result => console.log(result));
+// const promiseResolved = Promise.resolve({ id: 1 });
+// promiseResolved.then(result => console.log(result));
 
 // Create a Promise that is already rejected
+// const promiseRejected = Promise.reject(new Error('reason for rejection'));
+// promiseRejected.catch(error => console.log(error));
 
-const promiseRejected =  Promise.reject(new Error('reason for rejection'));
-promiseRejected.catch(error => console.log(error));
+// Running Promises in Parallel
+// Exclude 'reject' so we only have new Promise((resolve)...
+const p1 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Asynchronous operation 1 ...');
+        resolve(1);
+    }, 900);
+}); 
+
+// Include 'reject' so we have new Promise((resolve, reject)...
+/* const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Asynchronous operation 1 ...');
+        reject(new Error('because something failed'));
+    }, 1000);
+}); */
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Asynchronous operation 2 ...');
+        resolve(2);
+    }, 1000);
+});
+
+// Promise.all
+/* Promise.all([p1,p2])
+.then(result => console.log(result))
+.catch(err => console.log('Error: ', err.message)); */
+
+//See also Promise.race
+Promise.race([p1,p2])
+.then(result => console.log(result))
+.catch(err => console.log('Error: ', err.message));
